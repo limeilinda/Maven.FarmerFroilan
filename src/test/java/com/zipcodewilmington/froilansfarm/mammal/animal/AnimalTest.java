00d7e76b46df4800892da0971e24d870d7fced61
Jiayong Lin <96598259+linjiayo@users.noreply.github.com>
2022-07-25 18:28:34 -0400

    fix conflict (#8)

---
+++ src/test/java/com/zipcodewilmington/froilansfarm/mammal/animal/AnimalTest.java
@@ -0,0 +1,27 @@
+package com.zipcodewilmington.froilansfarm.mammal.animal;
+
+import com.zipcodewilmington.froilansfarm.product.edible.EarCorn;
+import com.zipcodewilmington.froilansfarm.product.edible.Edible;
+import com.zipcodewilmington.froilansfarm.product.edible.Tomato;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnimalTest {
+    @Test
+    public void testAnimal() {
+        Chicken chicken = new Chicken();
+        Tomato tomato = new Tomato();
+        EarCorn corn = new EarCorn();
+        chicken.eat(tomato);
+        chicken.eat(corn);
+
+        List<Edible> foodEaten = chicken.getFoodEaten();
+
+        Assert.assertEquals(tomato, foodEaten.get(0));
+        Assert.assertEquals(corn, foodEaten.get(1));
+    }
+}
